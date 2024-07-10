79e29c0dae81c08243f51de37249edcfc0b411ce
Breno Masato Matayoshi <145488931+BrenoMatayoshi@users.noreply.github.com>
2024-07-10 10:15:21 -0300

    Cabeçalho com menu prontos

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburgerMenu.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-});
+function toggleClass(element, className) {
+    element.classList.toggle(className);
+}
+  
+function menuToggle() {
+    const menu = document.querySelector('.menu-links');
+    if(menu.classList.contains('abriu')){
+        toggleClass(menu, 'fechar');
+    } else {
+        toggleClass(menu, 'abriu');
+    }
+}
