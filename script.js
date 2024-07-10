f82520ba71c253eb11134e7f7d4201cdbc880af2
Breno Masato Matayoshi <145488931+BrenoMatayoshi@users.noreply.github.com>
2024-07-10 11:18:47 -0300

    Otimizações no menu

--- script.js
+++ script.js
@@ -4,9 +4,5 @@ function toggleClass(element, className) {
   
 function menuToggle() {
     const menu = document.querySelector('.menu-links');
-    if(menu.classList.contains('abriu')){
-        toggleClass(menu, 'fechar');
-    } else {
-        toggleClass(menu, 'abriu');
-    }
+    toggleClass(menu, 'abriu');
 }
