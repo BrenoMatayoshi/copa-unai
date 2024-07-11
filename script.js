1a8f681dfc00e592fd045c85761c08707e0d5da9
Breno Masato Matayoshi <145488931+BrenoMatayoshi@users.noreply.github.com>
2024-07-11 10:12:19 -0300

    pequenas alterações

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+// Função generica para ativar/desativar uma classe. Parametros  (variavel da classe / Nova classe)
 function toggleClass(element, className) {
     element.classList.toggle(className);
 }
 
+// Função generica "clickar". Parametros (Classe do item / A classe nova)
 function Toggle(classSelector, newClass) {
     const toggle = document.querySelector(classSelector);
     toggleClass(toggle, newClass);
 }
+
+//  Animação no scroll    
+const Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        const index = Array.from(elements).indexOf(entry.target)
+        if(entry.isIntersecting){
+
+        } else {
+
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+const destaque = document.querySelectorAll('.botao-menu')
+
+elements.forEach((element) => {Observer.observe(element)})
+
