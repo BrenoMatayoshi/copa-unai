1cf656b828cf0110b391317eda85407f461f2a28
Breno Masato Matayoshi <145488931+BrenoMatayoshi@users.noreply.github.com>
2024-07-12 09:51:52 -0300

    Cronometro Pronto

--- script.js
+++ script.js
@@ -23,7 +23,42 @@ const Observer = new IntersectionObserver((entries) => {
 
 const elements = document.querySelectorAll('.hidden')
 
-const destaque = document.querySelectorAll('.botao-menu')
-
 elements.forEach((element) => {Observer.observe(element)})
 
+
+// Contagem regressiva
+const daysEl = document.querySelector('#days')
+const hoursEl = document.querySelector('#hours')
+const minutesEl = document.querySelector('#mins')
+const secondsEl = document.querySelector('#seconds')
+
+const targetDate = "31 jul 2024" // Define aqui a data limite (Dias Mês Ano)
+
+function countdown(){
+    const newTargetDate = new Date(targetDate)
+    const currentDate = new Date()
+
+    const totalSeconds = (newTargetDate - currentDate) / 1000
+
+    const days = Math.floor(totalSeconds / 3600 / 24)
+    const hours = Math.floor(totalSeconds / 3600) % 24
+    const mins = Math.floor(totalSeconds / 60) % 60
+    const seconds = Math.floor(totalSeconds) % 60
+
+    daysEl.innerHTML = days
+    hoursEl.innerHTML = formatTime(hours)
+    minutesEl.innerHTML = formatTime(mins)
+    secondsEl.innerHTML = formatTime(seconds)
+
+}
+
+function formatTime(time){
+    return time < 10 ? '0' + time : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000)
+
+
+// Imagens na página principal
